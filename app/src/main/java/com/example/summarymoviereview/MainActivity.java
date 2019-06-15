e6cb176eaace063dbb33275908c8c4d04ba187d7
incenger <dangtam.letan@gmail.com>
2019-06-15 13:30:12 +0700

    Add searchview and recyclerview to MainAcitivity

--- app/src/main/java/com/example/summarymoviereview/MainActivity.java
+++ app/src/main/java/com/example/summarymoviereview/MainActivity.java
@@ -8,6 +8,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.movie_cardview);
+        setContentView(R.layout.activity_main);
     }
 }
