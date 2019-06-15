37980923db8d4a4af8da6f7ee91851ce02f9551b
incenger <dangtam.letan@gmail.com>
2019-06-15 17:01:02 +0700

    Add MoiveInfo Activity

---
+++ app/src/main/java/com/example/summarymoviereview/MovieInfoActivity.java
@@ -0,0 +1,13 @@
+package com.example.summarymoviereview;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MovieInfoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_movie_info);
+    }
+}
