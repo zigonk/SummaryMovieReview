0eb5636ae43d788142e89c624f022589b98353f4
incenger <dangtam.letan@gmail.com>
2019-06-16 11:43:56 +0700

    Add Layout to MovieInfoActiviy

--- app/src/main/java/com/example/summarymoviereview/MovieInfoActivity.java
+++ app/src/main/java/com/example/summarymoviereview/MovieInfoActivity.java
@@ -1,7 +1,13 @@
 package com.example.summarymoviereview;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.cunoraz.tagview.Tag;
+import com.cunoraz.tagview.TagView;
+
+import java.util.ArrayList;
+
 
 public class MovieInfoActivity extends AppCompatActivity {
 
@@ -9,5 +15,16 @@ public class MovieInfoActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_movie_info);
+        ArrayList<Tag> tags = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            if (i % 3 == 0)
+                tags.add(new Tag("Oscar"));
+            else if (i % 2 == 0)
+                tags.add(new Tag("Not oscars"));
+            else
+                tags.add(new Tag("Fucking Long Text, I don't Know"));
+        }
+        TagView oscar = findViewById(R.id.movie_info_oscar);
+        oscar.addTags(tags);
     }
 }
