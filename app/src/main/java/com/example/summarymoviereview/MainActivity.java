160224a26e200d6c4e7f9a7b9fa6de5519d1b84a
Thuc Nguyen <zthucnguyen99@gmail.com>
2019-06-16 16:04:34 +0700

    feat: get entity sentiment and func combine entities

--- app/src/main/java/com/example/summarymoviereview/MainActivity.java
+++ app/src/main/java/com/example/summarymoviereview/MainActivity.java
@@ -19,7 +19,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         getSupportActionBar().setDisplayShowTitleEnabled(false);
-
     }
 
     @Override
